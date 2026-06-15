b971c37eeefb2c6af429f83695bd18c6ee6c92d3
ungdungthongminh-info <gustavjung01@gmail.com>
2026-06-15 11:04:18 +0700

    Add PWA cache reset service worker fix

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
+importScripts('/sw-fix.js');
