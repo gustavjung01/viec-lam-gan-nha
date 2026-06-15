2cf5aab0ca6fdbca5809e35cc4d2bd7c010d690b
gustavjung01 <gustavjung01@gmail.com>
2026-06-15 12:15:18 +0700

    Add standalone admin PWA React entry

---
+++ src/admin-main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './lib/aiInlineResultModalBridge';
+import './lib/companyLeadWorkflowBridge';
+import './lib/adminLeadWorkflowBridge';
+import './lib/adminLeadHistoryBridge';
+import { syncAdminPwaMetadata } from './lib/adminPwa';
+import { registerAdminPwaServiceWorker } from './lib/adminPwaRegistration';
+import { RoleProvider } from './contexts/RoleContext';
+
+syncAdminPwaMetadata('/admin/console');
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RoleProvider>
+      <App />
+    </RoleProvider>
+  </React.StrictMode>
+);
+
+registerAdminPwaServiceWorker();
